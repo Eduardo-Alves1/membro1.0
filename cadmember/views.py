d5d67c9f36be4bad25cf2cf43e036fd7bac5a89f
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-06-27 19:23:20 -0300

    Login_required

--- cadmember/views.py
+++ cadmember/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from cadmember.models import Member
 from cadmember.forms import MemberForm
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
 def members_view(request):
     members = Member.objects.all().order_by('name')
     search = request.GET.get('search')
@@ -9,6 +11,7 @@ def members_view(request):
         members = members.filter(name__icontains=search)
     return render(request, 'members.html', {'members': members})
 
+@login_required(login_url='/login')
 def new_member_view(request):
         if request.method == 'POST':
              new_member_form = MemberForm(request.POST)
