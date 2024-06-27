3364665f68e0510518397150539bc6f72c092b27
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-06-27 18:57:36 -0300

    Geral

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from cadmember.views import members_view,new_member_view
+from accounts.views import register_view, login_view,logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('members/',members_view, name='members_list'),
     path('new_member/',new_member_view, name='new_member'),
+    path('register/', register_view, name = 'register'),
+    path('login/', login_view, name = 'login'),
+    path('logout/', logout_view, name='logout'),  # from django.contrib.auth.views
 ]
