0920ebb3bcfb5089b2f45ce990358ea18e7e0757
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-06-28 20:44:48 -0300

    CreateView

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from cadmember.views import MemberListView, NewMemberView
+from cadmember.views import MembersListView, NewMemberCreateView
 from accounts.views import register_view, login_view,logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('members/', MemberListView.as_view(), name='members_list'),
-    path('new_member/',NewMemberView.as_view(), name='new_member'),
+    path('members/', MembersListView.as_view(), name='members_list'),
+    path('new_member/',NewMemberCreateView.as_view(), name='new_member'),
     path('register/', register_view, name = 'register'),
     path('login/', login_view, name = 'login'),
     path('logout/', logout_view, name='logout'),  # from django.contrib.auth.views
--- cadmember/views.py
+++ cadmember/views.py
@@ -1,17 +1,30 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from cadmember.models import Member
 from cadmember.forms import MemberForm
 from django.contrib.auth.decorators import login_required
-from django.views.generic import View
+from django.views import View
+from django.views.generic import ListView, CreateView
 
 
-class MemberListView(View):
-      def get(self, request,):
-            members = Member.objects.all().order_by('name')
-            search = request.GET.get('search')
-            if search:
-                members = members.filter(name__icontains=search)
-            return render(request,'members.html', {'members': members})
+class MembersListView(ListView):
+    model = Member
+    template_name ='members.html'
+    context_object_name ='members'
+    def get_queryset(self):
+        members = super().get_queryset().order_by('name')
+        search = self.request.GET.get('search')
+        if search:
+             members = members.filter(name__icontains=search)
+        return members
+#O código selecionado na classe `MembersListView` é responsável por lidar com a funcionalidade de pesquisa no aplicativo Django.
+
+#No método `get_queryset`, ele primeiro recupera todos os objetos `Member` do banco de dados usando o método `super().get_queryset()`. Em seguida, ele ordena os membros pelo nome usando o método `order_by('name')`.
+
+#Depois, ele verifica se uma consulta de pesquisa é fornecida nos parâmetros GET da solicitação. Se uma consulta de pesquisa for encontrada, ele filtra os membros com base no nome usando o método `filter(name__icontains=search)`. O lookup `icontains` executa uma pesquisa case-insensitive para a consulta de pesquisa fornecida dentro dos nomes dos membros.
+
+#Por fim, ele retorna os membros filtrados ou ordenados como o queryset para a ListView. Isso permite que a ListView exiba os resultados da pesquisa ou a lista completa de membros com base na consulta de pesquisa.
           
 
 
@@ -26,5 +39,11 @@ class NewMemberView(View):
                  new_member_form.save()
                  return redirect('members_list')
          return render(request, 'new_member.html', {'new_member_form': new_member_form})
+    
+class NewMemberCreateView(CreateView):
+     model = Member
+     form_class = MemberForm
+     template_name = 'new_member.html'    
+     success_url = '/members/'
          
       
