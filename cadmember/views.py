184c401ea4cd38a344bc1450e39919d6e2ae09de
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-06-28 18:17:18 -0300

    Refaturação para BaseView

--- cadmember/views.py
+++ cadmember/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render, redirect
 from cadmember.models import Member
 from cadmember.forms import MemberForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
 
-@login_required(login_url='/login')
-def members_view(request):
-    members = Member.objects.all().order_by('name')
-    search = request.GET.get('search')
-    if search:
-        members = members.filter(name__icontains=search)
-    return render(request, 'members.html', {'members': members})
 
-@login_required(login_url='/login')
-def new_member_view(request):
-        if request.method == 'POST':
-             new_member_form = MemberForm(request.POST)
-             if new_member_form.is_valid():
+class MemberListView(View):
+      def get(self, request,):
+            members = Member.objects.all().order_by('name')
+            search = request.GET.get('search')
+            if search:
+                members = members.filter(name__icontains=search)
+            return render(request,'members.html', {'members': members})
+          
+
+
+class NewMemberView(View):
+    def get(self, request):
+        new_member_form = MemberForm()
+        return render(request, 'new_member.html', {'new_member_form': new_member_form})
+    
+    def post(self, request):
+         new_member_form = MemberForm(request.POST)
+         if new_member_form.is_valid():
                  new_member_form.save()
                  return redirect('members_list')
-        else:
-             new_member_form = MemberForm()
-        return render(request, 'new_member.html', {'new_member_form': new_member_form})
-             
+         return render(request, 'new_member.html', {'new_member_form': new_member_form})
+         
+      
