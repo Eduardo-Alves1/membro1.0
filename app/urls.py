9a2da6e472055a45992c51b7916df3cda28a4a86
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-07-02 21:33:13 -0300

    Update, daleteView

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path
-from cadmember.views import MembersListView, NewMemberCreateView
+from cadmember.views import MembersListView, NewMemberCreateView, MemberUpdateView,MemberDeleteView
 from accounts.views import register_view, login_view,logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('members/', MembersListView.as_view(), name='members_list'),
     path('new_member/',NewMemberCreateView.as_view(), name='new_member'),
+    path('member/<int:pk>/update',MemberUpdateView.as_view(), name='member_update'),
+    path('member/<int:pk>/delete',MemberDeleteView.as_view(), name='member_delete'),
     path('register/', register_view, name = 'register'),
     path('login/', login_view, name = 'login'),
     path('logout/', logout_view, name='logout'),  # from django.contrib.auth.views
--- cadmember/views.py
+++ cadmember/views.py
@@ -1,13 +1,13 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render, redirect
 from cadmember.models import Member
 from cadmember.forms import MemberForm
 from django.contrib.auth.decorators import login_required
-from django.views import View
-from django.views.generic import ListView, CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class MembersListView(ListView):
     model = Member
     template_name ='members.html'
@@ -18,32 +18,23 @@ class MembersListView(ListView):
         if search:
              members = members.filter(name__icontains=search)
         return members
-#O código selecionado na classe `MembersListView` é responsável por lidar com a funcionalidade de pesquisa no aplicativo Django.
-
-#No método `get_queryset`, ele primeiro recupera todos os objetos `Member` do banco de dados usando o método `super().get_queryset()`. Em seguida, ele ordena os membros pelo nome usando o método `order_by('name')`.
-
-#Depois, ele verifica se uma consulta de pesquisa é fornecida nos parâmetros GET da solicitação. Se uma consulta de pesquisa for encontrada, ele filtra os membros com base no nome usando o método `filter(name__icontains=search)`. O lookup `icontains` executa uma pesquisa case-insensitive para a consulta de pesquisa fornecida dentro dos nomes dos membros.
-
-#Por fim, ele retorna os membros filtrados ou ordenados como o queryset para a ListView. Isso permite que a ListView exiba os resultados da pesquisa ou a lista completa de membros com base na consulta de pesquisa.
           
-
-
-class NewMemberView(View):
-    def get(self, request):
-        new_member_form = MemberForm()
-        return render(request, 'new_member.html', {'new_member_form': new_member_form})
-    
-    def post(self, request):
-         new_member_form = MemberForm(request.POST)
-         if new_member_form.is_valid():
-                 new_member_form.save()
-                 return redirect('members_list')
-         return render(request, 'new_member.html', {'new_member_form': new_member_form})
-    
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewMemberCreateView(CreateView):
      model = Member
      form_class = MemberForm
      template_name = 'new_member.html'    
      success_url = '/members/'
-         
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class MemberUpdateView(UpdateView):
+     model = Member
+     form_class = MemberForm
+     template_name = 'member_update.html'
+     success_url = '/members/'
+
+class MemberDeleteView(DeleteView):
+     model = Member
+     template_name ='member_delete.html'
+     success_url = '/members/'
       
