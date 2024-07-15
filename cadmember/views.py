4ef059bc1d63894c594e100aea914b85cad6c0c9
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-07-15 16:35:51 -0300

    Atualização def exportar

--- cadmember/views.py
+++ cadmember/views.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import openpyxl
 from django.http import HttpResponse
 from typing import Any
 from django.db.models.query import QuerySet
@@ -12,47 +13,87 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView, D
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class MembersListView(ListView):
     model = Member
-    template_name ='members.html'
-    context_object_name ='members'
+    template_name = 'members.html'
+    context_object_name = 'members'
+
     def get_queryset(self):
         members = super().get_queryset().order_by('name')
         search = self.request.GET.get('search')
         if search:
-             members = members.filter(name__icontains=search)
+            members = members.filter(name__icontains=search)
         return members
 
 
-    
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class NewMemberCreateView(CreateView):
-     model = Member
-     form_class = MemberForm
-     template_name = 'new_member.html'    
-     success_url = '/members/'
+    model = Member
+    form_class = MemberForm
+    template_name = 'new_member.html'
+    success_url = '/members/'
+
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class MemberUpdateView(UpdateView):
-     model = Member
-     form_class = MemberForm
-     template_name = 'member_update.html'
-     success_url = '/members/'
+    model = Member
+    form_class = MemberForm
+    template_name = 'member_update.html'
+    success_url = '/members/'
+
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class MemberDeleteView(DeleteView):
-     model = Member
-     template_name ='member_delete.html'
-     success_url = '/members/'
-     
+    model = Member
+    template_name = 'member_delete.html'
+    success_url = '/members/'
+
+
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class MemberDetailView(DetailView):
-     model = Member
-     template_name ='member_detail.html'
+    model = Member
+    template_name = 'member_detail.html'
+
 
 def exporta_excel(request):
-     members = Member.objects.all()
-     df = pd.DataFrame(members.values('name','cpf', 'date_birth', 'city_birth', 'state_birth', 'date_baptism', 'address', 'cep'))
-     response = HttpResponse(content_type='application/ms-excel')
-     response['Content-Disposition'] = 'attachment; filename=members.xlsx'
-     df.to_excel(response, index=False)
-     return response
+    members = Member.objects.all().order_by('name')
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    # Titulo da planilha
+    ws.title = "Membros"
 
+    headers = [
+        'ID',
+        'NOME',
+        'CPF',
+        'Data de Nascimento',
+        'Cidade de Nascimento',
+        'Estado Nascimento',
+        'Data de Batismo',
+        'Endereço',
+        'CEP',
+    ]
+
+    ws.append(headers)
+
+    for m in members:
+        ws.append(
+            [m.id,
+             m.name,
+             m.cpf,
+             m.date_birth.strftime('%d/%m/%Y'), 
+             m.city_birth.city,
+             m.state_birth.state, 
+             m.date_baptism.strftime('%d/%m/%Y'), 
+             m.address, 
+             m.cep]
+        )
+
+    # Configurando a resposta para fazer o download como um arquivo Excel
+    response = HttpResponse(
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    response["Content-Disposition"] = 'attachment; filename="membros.xlsx"'
+
+    # Salvando o workbook no buffer da resposta
+    wb.save(response)
+
+    return response
