57caaec69f56e91c735216eaa6f6595d7fa3b2b1
eduardoAlves <eduardo.santos.alves14@gamil.com>
2025-03-21 19:53:53 -0300

    Refactor settings.py to use SQLite for local development; update member_detail.html for improved layout and structure

--- app/settings.py
+++ app/settings.py
@@ -79,14 +79,6 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "member_db",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "member_db",
-        "PORT": "5432",
-    },
-    "dev": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
